e22034a4f8510713a7cfbe3e2efacdb02ad16bfc
shsangi <85803931+shsangi@users.noreply.github.com>
2025-12-26 09:37:34 +0500

    Update music_video.py

--- music_video.py
+++ music_video.py
@@ -15,7 +15,7 @@ from moviepy.editor import (
 
 # ---------- PAGE CONFIG ----------
 st.set_page_config(
-    page_title="Video Maker V 2:1",
+    page_title="Video Maker V 2:2", #update on any change
     layout="centered",
     initial_sidebar_state="collapsed"
 )
